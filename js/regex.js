3827cb7c49380b9a3ffa9dfeb5f6a14f701b9a6b
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-02-10 23:03:57 +0800

    Positive and Negative Lookahead

--- js/regex.js
+++ js/regex.js
@@ -1,7 +1,7 @@
 //  find the number of non-digit characters 
 
 let movieName = "2001: A Space Odyssey";
-let noNumRegex = /\D/g; // Change this line
+let noNumRegex = /\D/g; 
 let result = movieName.match(noNumRegex).length;
 console.log(result);
 
@@ -9,7 +9,7 @@ console.log(result);
 // Restrict Possible Usernames
 
 let username = "JackOfAllTrades";
-let userCheck = /^[a-z][a-z]+\d*$|^[a-z]\d\d+$/i; // Change this line
+let userCheck = /^[a-z][a-z]+\d*$|^[a-z]\d\d+$/i;
 let output = userCheck.test(username);
 console.log(output);
 
@@ -17,7 +17,7 @@ console.log(output);
 
 // Match-Whitespace
 let x = "Whitespace is important in separating words";
-let countWhiteSpace = /\s/g; // Change this line
+let countWhiteSpace = /\s/g;
 let a = x.match(countWhiteSpace);
 console.log(a);
 
@@ -27,6 +27,16 @@ console.log(a);
 // Match Non-Whitespace Characters
 
 let sample = "Whitespace is important in separating words";
-let countNonWhiteSpace = /\S/g; // Change this line
+let countNonWhiteSpace = /\S/g;
 let space = sample.match(countNonWhiteSpace);
 console.log(space);
+
+
+
+
+// Positive and Negative Lookahead
+
+let sampleWord = "astronaut";
+let pwRegex = /(?=\w{6})(?=\w*\d{2})/;
+let r = pwRegex.test(sampleWord);
+console.log(r);
