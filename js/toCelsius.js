e8a70f2bd29a11f7ddccbb14638e7eaf52a16dab
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-02-11 22:33:54 +0800

    Celsius to Fahrenheit

--- js/toCelsius.js
+++ js/toCelsius.js
@@ -1,8 +1,20 @@
 
-// JavaScript Function
+// Fahrenheit to Celsius
 
 function celsius(f) {
     return (5/9) * (f-32);
 }
 
 console.log(celsius(100));
+
+
+
+// Celsius to Fahrenheit
+
+function convertCtoF(celsius) {
+    let fahrenheit = celsius * (9/5) + 32;
+  
+    return fahrenheit;
+  }
+  
+  convertCtoF(30);
