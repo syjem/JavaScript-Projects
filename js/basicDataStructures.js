7301aa618549e6220a2cfe8e576a1e3e1ae9dc3b
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-02-11 21:06:20 +0800

    Check the Presence of an Element With indexOf()

--- js/basicDataStructures.js
+++ js/basicDataStructures.js
@@ -71,3 +71,14 @@ function spreadOut() {
     return sentence;
   }
   console.log(spreadOut());
+
+
+// Check For The Presence of an Element With indexOf()
+
+function quickCheck(arr, elem) {
+    if (arr.indexOf(elem) >= 0) {
+      return true;
+    }
+    return false;
+  }
+  console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
