b13e40aaf3fae689d0c4cfa24ee39a4207062cdf
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-02-11 19:59:47 +0800

    Add Items to an Array with push() and unshift()

---
+++ js/basicDataStructures.js
@@ -0,0 +1,10 @@
+
+// Add Items to an Array with push() and unshift()
+
+function mixedNumbers(arr) {
+  arr.unshift("I", 2, "three");
+  arr.push(7, "VIII", 9);
+  return arr;
+}
+
+console.log(mixedNumbers(['IV', 5, 'six']));
