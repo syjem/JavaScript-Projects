419891fbf430b940b203d6a072a8d53047cb4ddb
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-02-11 20:01:34 +0800

    Remove Items from an Array with pop() and shift()

--- js/basicDataStructures.js
+++ js/basicDataStructures.js
@@ -1,10 +1,18 @@
 
 // Add Items to an Array with push() and unshift()
-
 function mixedNumbers(arr) {
   arr.unshift("I", 2, "three");
   arr.push(7, "VIII", 9);
   return arr;
 }
-
 console.log(mixedNumbers(['IV', 5, 'six']));
+
+
+
+// Remove Items from an Array with pop() and shift()
+function popShift(arr) {
+    let popped = arr.pop();
+    let shifted = arr.shift(); 
+    return [shifted, popped];
+  }
+  console.log(popShift(['challenge', 'is', 'not', 'complete']));
