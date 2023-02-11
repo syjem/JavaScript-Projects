40b52870ca994171ec919df3c637f3fe188e18c4
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-02-11 21:47:32 +0800

    Access Property Names with Bracket Notation

--- js/basicDataStructures.js
+++ js/basicDataStructures.js
@@ -146,3 +146,21 @@ let userActivity = {
   };
   userActivity.data.online = 45;
   console.log(userActivity);
+
+
+
+//  Access Property Names with Bracket Notation
+
+let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28,
+    bananas: 13,
+    grapes: 35,
+    strawberries: 27
+  };
+  
+  function checkInventory(scannedItem) {
+    return(foods[scannedItem]);
+  }
+  console.log(checkInventory("apples"));
