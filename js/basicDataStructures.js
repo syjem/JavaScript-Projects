8dc04f659c0bbe7aa9d8b77c9fbb692f93c32280
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-02-11 22:17:22 +0800

    Generate an Array of  Object  with Object.keys()

--- js/basicDataStructures.js
+++ js/basicDataStructures.js
@@ -239,3 +239,31 @@ const u = {
   return(n);
   } 
   console.log(countOnline(u));
+
+
+// Generate an Array of All Object Keys with Object.keys()
+
+let k = {
+    Alan: {
+      age: 27,
+      online: false
+    },
+    Jeff: {
+      age: 32,
+      online: true
+    },
+    Sarah: {
+      age: 48,
+      online: false
+    },
+    Ryan: {
+      age: 19,
+      online: true
+    }
+  };
+  
+  function getArrayOfUsers(obj) {
+  
+      return(Object.keys(obj));
+  }
+  console.log(getArrayOfUsers(k));
