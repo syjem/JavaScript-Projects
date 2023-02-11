3d2d5233bc2630ae5074080bd51bf207157cf467
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-02-11 21:15:20 +0800

    Filtered version of the passed array

--- js/basicDataStructures.js
+++ js/basicDataStructures.js
@@ -98,3 +98,20 @@ function quickCheck(arr, elem) {
     return newArr;
   }
   greaterThanTen([2, 12, 8, 14, 80, 0, 1]);
+
+
+
+
+
+//  Filtered version of the passed array such that any array nested within arr containing elem has been removed.
+
+function filteredArray(arr, elem) {
+    let newArr = [];
+      for(let i=0; i<arr.length; i++) {
+        if (arr[i].indexOf(elem) == -1) {
+          newArr.push(arr[i]);
+        }
+      }
+    return newArr;
+  }
+  console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
